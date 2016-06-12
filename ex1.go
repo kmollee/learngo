ce1d564cb46743c702674e64c6af11a27d35b04d
kmollee <10073105@gm.nfu.edu.tw>
2016-06-12 19:26:20 +0800

    declear variable

--- ex1.go
+++ ex1.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+
+	// declar variable
+	var age int = 40
+	var favNum float64 = 1.6180339
+	fmt.Println(age, favNum)
+
+	var numOne = 1.000
+	var num99 = .9999
+	fmt.Println(numOne + num99)
+
 }
