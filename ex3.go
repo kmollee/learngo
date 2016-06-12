de0c834dc833853fc23ce0bc0724272c7835d902
kmollee <10073105@gm.nfu.edu.tw>
2016-06-12 20:01:34 +0800

    add if condition

--- ex3.go
+++ ex3.go
@@ -15,4 +15,12 @@ func main() {
 	for j := 0; j < 5; j++ {
 		fmt.Println(j)
 	}
+
+	// if condition
+	yourAge := 18
+	if yourAge >= 18 {
+		fmt.Println("You can drive")
+	} else {
+		fmt.Println("You can't drive")
+	}
 }
