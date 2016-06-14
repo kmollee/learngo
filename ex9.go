c69d0fca22d57a4df9a5b3c343eda2bcb84e9579
kmollee <10073105@gm.nfu.edu.tw>
2016-06-14 22:49:41 +0800

    about data convert

---
+++ ex9.go
@@ -0,0 +1,22 @@
+/*
+this example is about data convert
+*/
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	randInt := 5
+	randFloat := 10.5
+	randString := "100"
+	// randString2 := "250.5"
+
+	fmt.Println(float64(randInt))
+	fmt.Println(int(randFloat))
+
+	newInt, _ := strconv.ParseInt(randString, 0, 64)
+	fmt.Println("newInt=", newInt)
+}
