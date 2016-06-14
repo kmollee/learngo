2b33ce38d4d4afba649e81dd442b07462bef8afd
kmollee <10073105@gm.nfu.edu.tw>
2016-06-14 19:51:03 +0800

    functions example

---
+++ ex6.go
@@ -0,0 +1,41 @@
+/*
+examples for function
+*/
+package main
+
+import "fmt"
+
+// sum up all element of numbers
+func addThemUp(numbers []float64) float64 {
+	sum := 0.0
+
+	for _, val := range numbers {
+		sum += val
+	}
+	return sum
+}
+
+// return next 2 value of number
+func next2Values(number int) (int, int) {
+	return number + 1, number + 2
+}
+
+// undefine number of function
+// subtract
+func subtractThem(args ...int) int {
+	finalValue := 0
+	for _, val := range args {
+		finalValue -= val
+	}
+	return finalValue
+}
+
+func main() {
+	listNums := []float64{1, 2, 3, 4, 5}
+	fmt.Println("Sum :", addThemUp(listNums))
+
+	num1, num2 := next2Values(5)
+	fmt.Println(num1, num2)
+
+	fmt.Println(subtractThem(1, 2, 3, 4, 5))
+}
